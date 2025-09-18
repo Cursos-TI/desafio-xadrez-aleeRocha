d3733d49c4439b09bc3cb715f9bfbdaf8c538513
ALEXANDRE ROCHA FARIA <aleerochafaria@gmail.com>
2025-09-18 17:31:20 +0000

    Foi implementado as estruturas de repetição for, while e do while para movimentar as peças de xadrez.

--- xadrez.c
+++ xadrez.c
@@ -5,18 +5,6 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
@@ -27,6 +15,44 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+
+    int casasTorre = 5, torre;
+    int casasBispo = 5, bispo = 1;
+    int casasRainha = 8, rainha = 1;
+
+
+    printf("**** Desafio movimentação de peças de xadrez ****\n");
+    printf("\n");
+
+    
+    printf("Movimentação da peça torre, cinco casas para a direita.\n");
+
+    // Movimento da peça torre, cinco casas para a direita utilizando a estrutura de repetição for.
+    for (torre = 1; torre <= casasTorre; torre++)
+    {
+        printf("Direita.\n");
+    }
+
+
+    printf("\nMovimentação da peça bispo, cinco casas na diagonal.\n");
+
+    // Movimento da peça bispo, cinco casas na diagonal utilizando a estrutura de repetição while.
+    while (bispo <= casasBispo)
+    {
+        printf("Cima, Direita.\n");
+        bispo++;
+    }
+
+
+    printf("\nMovimentação da peça rainha, oito casas para a esquerda.\n");
+
+    // Movimento da peça rainha, oito casas para a esquerda utilizando a estrutura de repetição do while.
+    do
+    {
+        printf("Esquerda.\n");
+        ++rainha;
+    } while (rainha <= casasRainha);
+    
 
     return 0;
 }
